f0fe5fd8a9358b4023014e88d1f314e7fbf4a7dc
Victor Moreno <r07k1v@gmail.com>
2021-04-17 14:52:11 -0500

    Delivery tracker project added

---
+++ DeliveryTracker/main.js
@@ -0,0 +1,52 @@
+function createTR(time_frame) {
+  let table = document.getElementById("table");
+  let node = document.createElement("tr");
+  node.setAttribute("id", time_frame);
+  let nodec = document.createElement("td");
+  nodec.innerHTML = time_frame;
+  node.appendChild(nodec);
+  createBtn(node, time_frame, "monday");
+  createBtn(node, time_frame, "tuesday");
+  createBtn(node, time_frame, "wednesday");
+  createBtn(node, time_frame, "thursday");
+  createBtn(node, time_frame, "friday");
+  table.appendChild(node);
+}
+
+function createBtn(node, time_frame, day) {
+  let btnNode = document.createElement("button");
+  btnNode.innerHTML = "8";
+  let nodetd = document.createElement("td");
+  nodetd.setAttribute("id", time_frame + day);
+  btnNode.setAttribute("id", "btn" + time_frame + day);
+  btnNode.setAttribute(
+    "onclick",
+    "takeOne('" + btnNode.getAttribute("id") + "');"
+  );
+  btnNode.style = "color:white;background-color:green";
+  nodetd.appendChild(btnNode);
+  node.appendChild(nodetd);
+}
+
+for (i = 8; i <= 20; i++) {
+  if (i < 10) {
+    createTR("0" + i + ":00");
+    createTR("0" + i + ":30");
+  } else {
+    createTR(i + ":00");
+    createTR(i + ":30");
+  }
+}
+
+function takeOne(id) {
+  let btn = document.getElementById(id);
+  let cnt = btn.innerHTML;
+  if (cnt > 0) {
+    cnt = cnt - 1;
+    btn.innerHTML = cnt;
+    if (cnt <= 0) {
+      btn.style = "color:white; background-color:red";
+      console.log("no more delivery guys!");
+    }
+  }
+}
